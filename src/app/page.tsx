6c0ab8df816af87556fed90fc31b886a1b1e21df
dev-mansi-thakkar <166789401+dev-mansi-thakkar@users.noreply.github.com>
2024-04-12 15:52:36 +0000

    resolved path issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Login from "../components/Login/Login";
+import Login from "../components/login/Login";
 import ErrorBoundary from "../pages/errorBoundary";
 export default function Home() {
   return (
